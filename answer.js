97f7fa9d4bf1933d64207ec589a824c8b29d3dd8
umubyeyialice7@gmail.com <umubyeyialice@gmail.com>
2024-03-04 16:30:48 +0200

    question and answer

---
+++ answer.js
@@ -0,0 +1,32 @@
+///// Create a function that returns the sum of 2 numbers///
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+let result = addNumbers(5, 10);
+console.log(result); // This will output 15
+
+////////Create a function that returns the product of 2 numbers//////
+function multiply(one, two) {
+    return one * two;
+}
+
+let resul = multiply(6, 9);
+console.log(resul); // This will output 50
+
+////////  Create a function that returns the difference of 2 numbers/////
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+let resu = subtract(10, 5);
+console.log(resu); // This will output 5
+////////Create a function that returns the division of 2 numbers. //////
+ function division(div1,div2){
+    return div1 % div2;
+ }
+ let divi=division(10,5)
+ console.log(divi);
+
+
+
+
+    
